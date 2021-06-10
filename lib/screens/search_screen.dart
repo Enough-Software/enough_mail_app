d4314ee91e1bb17598e00794576f5e6faed10483
Robert Virkus <robert.virkus@enough.de>
2021-06-10 18:02:12 +0200

    initial cupertino search screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,21 +1,61 @@
+import 'package:enough_mail/enough_mail.dart';
+import 'package:enough_mail_app/locator.dart';
+import 'package:enough_mail_app/models/models.dart';
+import 'package:enough_mail_app/routes.dart';
+import 'package:enough_mail_app/services/navigation_service.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 /// Search input screen, currently used only for Cupertino
 class SearchScreen extends StatefulWidget {
-  SearchScreen({Key key}) : super(key: key);
+  final MessageSource messageSource;
+  SearchScreen({Key key, @required this.messageSource}) : super(key: key);
 
   @override
   _SearchScreenState createState() => _SearchScreenState();
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    //TODO load previous search texts
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final localizations = AppLocalizations.of(context);
     return Row(
       children: [
-        CupertinoSearchTextField(),
-        CupertinoDialogAction(child: Text(cancel))
+        Expanded(
+          child: CupertinoSearchTextField(
+            controller: _controller,
+            onSubmitted: _onSearchSubmitted,
+          ),
+        ),
+        CupertinoDialogAction(
+          child: Text(localizations.actionCancel),
+          onPressed: () => Navigator.of(context).pop(),
+        )
       ],
+    );
+  }
+
+  void _onSearchSubmitted(String text) {
+    final search = MailSearch(text, SearchQueryType.allTextHeaders);
+    final next = widget.messageSource.search(search);
+    locator<NavigationService>().push(
+      Routes.messageSource,
+      arguments: next,
+      replace: true,
     );
   }
 }
